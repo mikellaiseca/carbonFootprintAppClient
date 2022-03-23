ad3a5e7f6edabef20ceaf2e248566b5c00a9c4b0
mikellaiseca <mikellaiseca@gmail.com>
2022-03-23 18:50:22 +0100

    d

--- src/services/news.service.js
+++ src/services/news.service.js
@@ -4,7 +4,7 @@ class NewsService {
 
     constructor() {
         this.axiosApp = axios.create({
-            baseURL: 'http://api.mediastack.com/v1/'
+            baseURL: 'https://api.mediastack.com/v1/'
         })
     }
 
